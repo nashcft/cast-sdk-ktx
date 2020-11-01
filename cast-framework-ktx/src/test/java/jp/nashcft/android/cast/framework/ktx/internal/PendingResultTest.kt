d3bc7861bdb48b8c58169aef905f456ea97ee2cf
nashcft <nashrfv@gmail.com>
2020-11-01 14:31:08 +0900

    fix test

--- cast-framework-ktx/src/test/java/jp/nashcft/android/cast/framework/ktx/internal/PendingResultTest.kt
+++ cast-framework-ktx/src/test/java/jp/nashcft/android/cast/framework/ktx/internal/PendingResultTest.kt
@@ -17,7 +17,32 @@ class PendingResultTest {
   @Test
   fun `when the given continuation is successfully resumed`(): Unit = runBlocking {
     val result = suspendCancellableCoroutine<Result> {
-      awaitPendingResult(it) { mockPendingResult }
+      awaitPendingResult(it) {
+        object : PendingResult<Result>() {
+
+          private val result = Result { Status(CommonStatusCodes.SUCCESS) }
+
+          override fun await(): Result = result
+
+          override fun await(p0: Long, p1: TimeUnit): Result = result
+
+          override fun cancel() {
+          }
+
+          override fun isCanceled(): Boolean = false
+
+          override fun setResultCallback(p0: ResultCallback<in Result>) {
+            p0.onResult(result)
+          }
+
+          override fun setResultCallback(
+            p0: ResultCallback<in Result>,
+            p1: Long,
+            p2: TimeUnit
+          ) {
+          }
+        }
+      }
     }
 
     assertThat(result.status.statusCode).isEqualTo(CommonStatusCodes.SUCCESS)
@@ -31,40 +56,42 @@ class PendingResultTest {
     error("Should not be reached here")
   }
 
-  @Test(expected = CancellationException::class)
+  @Test
   fun `when the given continuation has been cancelled before resume`(): Unit = runBlocking {
-    suspendCancellableCoroutine<Result> {
-      awaitPendingResult(it) {
-        it.cancel()
-        mockPendingResult
+    var called = false
+    try {
+      suspendCancellableCoroutine<Result> { cont ->
+        awaitPendingResult(cont) {
+          object : PendingResult<Result>() {
+
+            private var result = Result { Status(CommonStatusCodes.SUCCESS) }
+
+            override fun await(): Result = result
+
+            override fun await(p0: Long, p1: TimeUnit): Result = result
+
+            override fun cancel() {
+              called = true
+            }
+
+            override fun isCanceled(): Boolean = false
+
+            override fun setResultCallback(p0: ResultCallback<in Result>) {
+              cont.cancel()
+            }
+
+            override fun setResultCallback(
+              p0: ResultCallback<in Result>,
+              p1: Long,
+              p2: TimeUnit
+            ) {
+            }
+          }
+        }
       }
-    }
-    error("Should not be reached here")
-  }
-
-  private val mockPendingResult = object : PendingResult<Result>() {
-
-    private val result = Result { Status(CommonStatusCodes.SUCCESS) }
-
-    override fun await(): Result = result
-
-    override fun await(p0: Long, p1: TimeUnit): Result = result
-
-    override fun cancel() {
-    }
-
-    override fun isCanceled(): Boolean = false
-
-    override fun setResultCallback(p0: ResultCallback<in Result>) {
-      p0.onResult(result)
-    }
-
-    override fun setResultCallback(
-      p0: ResultCallback<in Result>,
-      p1: Long,
-      p2: TimeUnit
-    ) {
-      p0.onResult(result)
+    } catch (th: Throwable) {
+      assertThat(th).isInstanceOf(CancellationException::class.java)
+      assertThat(called).isTrue()
     }
   }
 }
