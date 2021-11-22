a8ae5b685266b2291c9d2e6729f0b871c5993eb4
nashcft <nashrfv@gmail.com>
2021-11-22 19:00:01 +0900

    modify JVM target to 1.8

--- buildSrc/src/main/java/Android.kt
+++ buildSrc/src/main/java/Android.kt
@@ -16,12 +16,12 @@ fun LibraryExtension.commonConfig() {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
   }
 
   kotlinOptions {
-    jvmTarget = "11"
+    jvmTarget = "1.8"
   }
 
   buildFeatures {
